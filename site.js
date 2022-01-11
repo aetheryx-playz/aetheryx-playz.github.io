10f7e91fec84455177568b5a479ca7c8a21635ee
aetheryx-playz <aetheryx@seemslegit.email>
2022-01-11 19:25:59 +0000

    atom

--- site.js
+++ site.js
@@ -13,3 +13,20 @@
     signIn.classList.remove("active");
     signUp.classList.remove("inActive");
 }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
