97369b0a86daf10258529280a84f0ef8fb548fd1
aetheryx-playz <aetheryx@seemslegit.email>
2022-01-14 19:08:08 +0000

    atom

--- site.js
+++ site.js
@@ -42,7 +42,8 @@ function onSignIn(googleUser) {
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
-//google sign outline  function signOut() {
+//google sign out
+    line function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
