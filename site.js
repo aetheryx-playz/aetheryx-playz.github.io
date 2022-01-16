276b333b7b0d62acf378df4358f8b91d778c0e9e
aetheryx-playz <aetheryx@seemslegit.email>
2022-01-16 16:00:51 +0000

    atom

--- site.js
+++ site.js
@@ -1,19 +1,3 @@
-//login
- let btnLogin = document.getElementById("login");
- let btnSignUp = document.getElementById("signup");
-
- let signIn = document.querySelector(".signin");
- let signUp = document.querySelector(".signup");
-
- btnLogin.onclick = function(){
-     signIn.classList.add("active");
-     signUp.classList.add("inActive");
- }
-
- btnSignUp.onclick = function(){
-    signIn.classList.remove("active");
-    signUp.classList.remove("inActive");
-}
 
 //dropdown
 function myFunction() {
@@ -32,20 +16,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-//google sign in
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-//google sign out
-line function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
