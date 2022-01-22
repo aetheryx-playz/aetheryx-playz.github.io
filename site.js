cff1ce0a0e0672c0a74138c74cea4aa91aabb1b1
aetheryx-playz <aetheryx@seemslegit.email>
2022-01-22 14:29:17 +0000

    atom

--- site.js
+++ site.js
@@ -1,4 +1,14 @@
-function bodylightmode() {
-   var element = document.body;
-   element.classList.toggle("light-mode");
+let root = document.documentElement;
+document.querySelector(".js-dark-mode-toggle").addEventListener("click", function() {
+  darkmodeToggle();
+});
+
+function darkmodeToggle() {
+  root.classList.toggle("theme--dark");
+  if (root.classList.contains("theme--dark")) {
+    localStorage.setItem("mode", "dark");
+  }
+  else {
+    localStorage.setItem("mode", "light");
+  }
 }
