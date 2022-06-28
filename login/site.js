87e8b1185d3d26f73fe290444d40edc3aca81083
aetheryx-playz <aetheryx@seemslegit.email>
2022-06-28 18:50:19 +0100

    atom

--- login/site.js
+++ login/site.js
@@ -1,11 +1,10 @@
 //google sign in
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('ID: ' + profile.getId());
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+  console.log('Email: ' + profile.getEmail()); 
 
 //google sign out
 line function signOut() {
